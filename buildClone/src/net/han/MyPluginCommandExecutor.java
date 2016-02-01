7a79615bf132e3bb11da7ac9632e8f54f88e6ab5
cndavy <cndavy@163.com>
2016-02-01 08:30:20 +0800

    mc plugin  clone build copy v m

--- buildClone/src/net/han/MyPluginCommandExecutor.java
+++ buildClone/src/net/han/MyPluginCommandExecutor.java
@@ -7,11 +7,16 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
 
 /**
  * Created by han on 2016/1/30.
@@ -29,7 +34,7 @@ public class MyPluginCommandExecutor implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (args.length > 5) {
+ /*       if (args.length > 5) {
             sender.sendMessage("Too many arguments!");
             return false;
         }
@@ -37,7 +42,7 @@ public class MyPluginCommandExecutor implements CommandExecutor {
             sender.sendMessage("Not enough arguments!");
             return false;
         }
-
+*/
      /*   Player target = (Bukkit.getServer().getPlayer(args[0]));
         if (target == null) {
             sender.sendMessage(args[0] + " is not online!");
@@ -48,17 +53,17 @@ public class MyPluginCommandExecutor implements CommandExecutor {
         String  X= args[1];
         String  Y =args[2];
         String  Z=args[3];
-        plugin.getLogger().info(cmdCV + " X=" + X + ",Y=" + Y + ",Z=" + Z);
+    //    plugin.getLogger().info(cmdCV + " X=" + X + ",Y=" + Y + ",Z=" + Z);
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (cmd.getName().equalsIgnoreCase("xiaoxi")) {
-                player = (Player) sender;
+
                 plugin.getLogger().info(player.getDisplayName() + " use command xiaoxi");
                 world = player.getWorld();
                 location = player.getLocation();
                 inventory = player.getInventory();
                 if (args[0].equalsIgnoreCase("m"))
-                    generateCube(  location,Integer.parseInt(args[1]));
+                    generateCube(  location,Integer.parseInt(args[1]),Integer.parseInt(args[2]),Integer.parseInt(args[3]));
 
                 if (args[0].equalsIgnoreCase("c"))
                     copyCube(location, Integer.parseInt(args[1]), Integer.parseInt(args[2]),Integer.parseInt(args[3]));
@@ -114,7 +119,9 @@ private List<LocCub> cvList =null;
            Block currentBlock = world.getBlockAt(loc.getBlockX()+locCub.getX(),
                    loc.getBlockY()+locCub.getY(),
                    loc.getBlockZ()+locCub.getZ());
-
+           if (locCub.getX()>X) continue;
+           if (locCub.getY()>Y) continue;
+           if (locCub.getY()>Y) continue;
            plugin.getLogger().info(" X=" + (loc.getBlockX()+locCub.getX()) + "," +
                                     " Y=" + (loc.getBlockY()+locCub.getY()) + "," +
                                     " Z=" + (loc.getBlockZ()+locCub.getZ()));
@@ -124,7 +131,7 @@ private List<LocCub> cvList =null;
 
 
     }
-    public void generateCube(Location loc, int length) {
+    public void generateCube(Location loc, int x, int y, int z) {
         // Set one corner of the cube to the given location.
         // Uses getBlockN() instead of getN() to avoid casting to an int later.
         int x1 = loc.getBlockX();
@@ -132,9 +139,9 @@ private List<LocCub> cvList =null;
         int z1 = loc.getBlockZ();
 
         // Figure out the opposite corner of the cube by taking the corner and adding length to all coordinates.
-        int x2 = x1 + length;
-        int y2 = y1 + length;
-        int z2 = z1 + length;
+        int x2 = x1 + x;
+        int y2 = y1 + y;
+        int z2 = z1 + z;
 
         World world = loc.getWorld();
 
@@ -151,5 +158,69 @@ private List<LocCub> cvList =null;
                 }
             }
         }
+    }
+
+    private static double func1(double num1,double num2){
+        if(num1<0)return num1+num2;
+        return num1-num2;
+    }
+    private static double disFromCenter(Vector v){
+        return Math.sqrt(v.getX()*v.getX()+v.getY()*v.getY()+v.getZ()*v.getZ());
+    }
+    private static void weaken(Vector v,double wval){
+        double dis=disFromCenter(v);
+        v.setX(func1(v.getX(),(wval/dis)*v.getX()));
+        v.setY(func1(v.getY(),(wval/dis)*v.getY()));
+        v.setZ(func1(v.getZ(),(wval/dis)*v.getZ()));
+    }
+    private static double distance(Location loc,double x,double y,double z){
+        return Math.sqrt((loc.getX()-x)*(loc.getX()-x)+(loc.getY()-y)*(loc.getY()-y)+(loc.getZ()-z)*(loc.getZ()-z));
+    }
+
+    public static List<Entity> explore(Location loc,int damage,int raidus){
+
+        World hdw=loc.getWorld();
+
+        List<Entity> ret=new ArrayList<Entity>();
+        Collection<Entity> sch=loc.getWorld().getNearbyEntities(loc,raidus,raidus,raidus);
+        for(Entity e:sch){
+            if(loc.distance(e.getLocation())<=raidus)ret.add(e);
+        }
+        for(int x=loc.getBlockX()-raidus;x<=loc.getBlockX()+raidus;x++)
+            for(int y=loc.getBlockY()-raidus;y<=loc.getBlockY()+raidus;y++)
+                for(int z=loc.getBlockZ()-raidus;z<=loc.getBlockZ()+raidus;z++){
+                    if(distanceSquared(loc,x,y,z)<=raidus*raidus){
+                        Block b=hdw.getBlockAt(x,y,z);
+                        if(!b.getType().isBlock())continue;
+                        if(b.getType().getMaxDurability()>(short)(damage-damage*((distance(loc,x,y,z)/raidus))))continue;
+                        Location bkat=b.getLocation();
+                        Material type=b.getType();
+                        if(b.breakNaturally(null)){
+                            FallingBlock ent=hdw.spawnFallingBlock(bkat,type,(byte) 0);
+                            ret.add(ent);
+                        }
+                    }
+                }
+        for(Entity e:ret){
+            Vector v=e.getVelocity();//
+            Vector v2=e.getLocation().subtract(loc).toVector();
+            double dis=e.getLocation().distance(loc);
+            double proportion=(double)(raidus)/dis;
+            v2.setX(v2.getX()*proportion);
+            v2.setY(v2.getY()*proportion);
+            v2.setZ(v2.getZ()*proportion);
+            weaken(v2,dis);
+            v2.multiply(0.2f);
+            v.add(v2);
+            e.setVelocity(v);
+        }
+
+        return ret;
+
+        //TODO
+
+    }
+    private static double distanceSquared(Location loc, int x, int y, int z) {
+        return (loc.getX()-x)*(loc.getX()-x)+(loc.getY()-y)*(loc.getY()-y)+(loc.getZ()-z)*(loc.getZ()-z);
     }
 }
